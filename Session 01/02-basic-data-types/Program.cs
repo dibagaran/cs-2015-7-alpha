49f3d73f6b0f1e87091f1ecfb57f93cbef53b5c5
Meysam Mousavi <xeronith@gmail.com>
2015-07-13 16:19:45 +0430

    //

--- Session 01/02-basic-data-types/Program.cs
+++ Session 01/02-basic-data-types/Program.cs
@@ -1,19 +1,19 @@
 public class Program{
 	public static void Main()
 	{
-		byte a; // Byte
-		sbyte b; // SByte
-		short c; // Int16
-		ushort d; // UInt16
-		int e; // Int32
-		uint f; // UInt32
-		long g; // Int64
-		ulong h; // UInt64
-		float i; // Single
-		double j; // Double
-		decimal k; // Decimal
-		char l; // Char
-		string m; //String
-		bool n; //Boolean
+		/* Byte */ byte a; // 0 .. 255
+		/* SByte */ sbyte b; // -128 .. 127
+		/* Int16 */ short c; // -32,768 .. 32,767
+		/* UInt16 */ ushort d; // 0 .. 65,535
+		/* Int32 */ int e; // -2,147,483,648 .. 2,147,483,647
+		/* UInt32 */ uint f; // 0 .. 4,294,967,295
+		/* Int64 */ long g; // -9,223,372,036,854,775,808 .. 9,223,372,036,854,775,807
+		/* UInt64 */ ulong h; // 0 .. 18,446,744,073,709,551,615
+		/* Single */ float i; // -3.402823e38 .. 3.402823e38
+		/* Double */ double j; // -1.79769313486232e308 .. 1.79769313486232e308
+		/* Decimal */ decimal k; // -79228162514264337593543950335 .. 79228162514264337593543950335
+		/* Char */ char l; // A Unicode character.
+		/* String */ string m; // A string of Unicode characters.
+		/* Boolean */ bool n; // true or false.
 	}
 }
