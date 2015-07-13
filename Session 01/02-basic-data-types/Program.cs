e62d1eead87ca2ba168d5a2e292f659f4c994d01
Meysam Mousavi <xeronith@gmail.com>
2015-07-13 16:11:58 +0430

    //

---
+++ Session 01/02-basic-data-types/Program.cs
@@ -0,0 +1,19 @@
+public class Program{
+	public static void Main()
+	{
+		byte a; // Byte
+		sbyte b; // SByte
+		short c; // Int16
+		ushort d; // UInt16
+		int e; // Int32
+		uint f; // UInt32
+		long g; // Int64
+		ulong h; // UInt64
+		float i; // Single
+		double j; // Double
+		decimal k; // Decimal
+		char l; // Char
+		string m; //String
+		bool n; //Boolean
+	}
+}
