8636bd43abdc619d9c3ecb92937ff6b27ac29940
Meysam Mousavi <xeronith@gmail.com>
2015-07-23 11:36:15 +0430

    //

--- Session 05/03-objects/Program.cs
+++ Session 05/03-objects/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main()
     {
         var i = new Person (); // or Person i = new Person();
-        var j = i; // All objects are reference types like arrays so i and j are references to the same object.
+        var j = i; // Like arrays, all objects are reference types so i and j are references to the same object.
 
         var k = new Person (); // k is another instance of Person class.
     }
