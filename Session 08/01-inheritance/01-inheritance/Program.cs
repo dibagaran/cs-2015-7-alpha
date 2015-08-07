53418796063631e1db8e7f4739d4bee5fda276f8
Meysam Mousavi <xeronith@gmail.com>
2015-08-07 17:14:18 +0430

    //

---
+++ Session 08/01-inheritance/01-inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    public static void Main ()
+    {
+    }
+}
+
+class OperatingSystem{
+}
+
+class MobileOperatingSystem : OperatingSystem{ // A MobileOperatingSystem IS an OperatingSystem
+}
+
+class Linux : OperatingSystem{ // Linux IS an OperatingSystem
+}
+
+class Windows : OperatingSystem{ // Windows IS an OperatingSystem
+}
+
+class Android : MobileOperatingSystem{ // Android IS a MobileOperatingSystem
+}
---
+++ Session 08/02-constructor-chaining/02-constructor-chaining/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    public static void Main (string[] args)
+    {
+        var rectangle = new Rectangle (10, 20);
+        var square = new Square (10);
+        var unitSquare = new UnitSquare ();
+
+        Console.WriteLine (rectangle.Area);
+        Console.WriteLine (square.Area);
+        Console.WriteLine (unitSquare.Area);
+    }
+}
+
+class Rectangle{
+    
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(int width, int height)
+    {
+        this.Width = width;
+        this.Height = height;
+    }
+        
+    public double Area{
+        get{
+            return this.Width * this.Height;
+        }
+    }
+}
+
+class Square : Rectangle{
+
+    public Square(int dimension) : base(dimension, dimension)
+    {
+    }
+}
+
+class UnitSquare : Square{
+
+    public UnitSquare() : base(1)
+    {
+    }
+}
