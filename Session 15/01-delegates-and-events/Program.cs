a330c8ebbd0770f0a28ad7886e0aef18f0de58d7
Meysam Mousavi <xeronith@gmail.com>
2015-08-31 15:26:24 +0430

    //

---
+++ Session 15/01-delegates-and-events/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    public static void Main ()
+    {
+        var store = new Store ();
+
+        //Handling the event
+        store.ProductAdded += OnProductAdded;
+
+        store.Add (new Product ());
+    }
+
+    static void OnProductAdded ()
+    {
+        Console.WriteLine ("A new product has been added.");
+    }
+}
+
+class Store
+{
+    public delegate void ProductHandler ();
+    public event ProductHandler ProductAdded;
+
+    private Product[] products = new Product[1000];
+    private int current = 0;
+
+    public void Add (Product product)
+    {
+        products [current++] = product;
+
+        //Raising or firing the event
+        if (ProductAdded != null)
+            ProductAdded ();
+    }
+}
+
+class Product
+{
+}
